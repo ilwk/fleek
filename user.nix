4b40720af075b87a2370c166f993d113b726fc3a
vet3x <vet3x@outlook.com>
2023-07-04 22:39:50 +0800

    fleek: apply

--- user.nix
+++ user.nix
@@ -3,10 +3,13 @@
   programs.zsh = {
     enable = true;
 
-    zplug = [
-      { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-      { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-    ];
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+      ];
+    };
   };
  
 }
