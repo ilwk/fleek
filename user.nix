98007636a1d1d76ef5370fccc6979f60bab1dc27
vet3x <vet3x@outlook.com>
2023-07-08 00:19:52 +0800

    fleek: apply

--- user.nix
+++ user.nix
@@ -1,7 +1,7 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
   programs.bash = {
-    initExtra = "zsh\ncd ~";
+    initExtra = "cd ~\nzsh";
   };
 
   programs.zsh = {
