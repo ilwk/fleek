403710feb6b9c29eafab82f275c631db33d9c2f0
vet3x <vet3x@outlook.com>
2023-07-08 00:02:22 +0800

    fleek: apply

--- programs.nix
+++ programs.nix
@@ -9,7 +9,6 @@
     programs.starship.enable = true;
 
   # User specified programs 
-    programs.dircolors.enable = true; 
-    programs.starship.enable = true;
+    programs.dircolors.enable = true;
 
 }
